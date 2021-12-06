da389492f6c6e6178e2a56afec26148aa04f9090
jh177 <jameshong177@gmail.com>
2021-12-06 14:19:22 -0800

    Delete dupblicated set up

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => res.send("Hello World"));
-
-const port = process.env.PORT || 6000;
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
