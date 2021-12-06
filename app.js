bfc3a597e7439430ed8ebd88fef543e403b7cabf
Alan Chiu <ms0372631@gmail.com>
2021-12-06 14:38:17 -0800

    Created keys.js

--- app.js
+++ app.js
@@ -7,4 +7,4 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listen on port ${port}`));
+app.listen(port, () => {console.log(`Listen on port ${port}`)});
