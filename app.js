97a85b77afa7553d00b381f89ff54ef077ec2e3f
Alan Chiu <ms0372631@gmail.com>
2021-12-06 14:42:29 -0800

    Created keys.js

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express  = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true})
+  .then(() => console.log("Connected to mongoDB"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
