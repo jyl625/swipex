a90d33f680803d91df41c1179b7744964202e8e2
Alan Chiu <ms0372631@gmail.com>
2021-12-06 14:45:45 -0800

    Added server:debug

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express  = require("express");
+const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
