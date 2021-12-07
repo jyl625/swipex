f4d600da8fdbfcb006a99bb5d75bb3e900a900cc
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-12-06 21:30:24 -0800

    Create keys_dev and keys_prod for heroku push

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://admin:SKdc5ODGQqsM627M@cluster0.vbtad.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  secretOrKey: 'Abeji49FMZ'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
