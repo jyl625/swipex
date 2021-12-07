ce773fd7b0f7ca32c1c54b16dfd552c89899abad
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-12-06 18:13:00 -0800

    Add api util, actions, and reducers related to sessions

---
+++ frontend/src/reducers/root_reducer.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+import session from './session_reducer';
+import errors from './errors_reducer'; 
+import swipes from './swipes_reducer'
+
+const RootReducer = combineReducers({
+  session,
+  errors, 
+  swipes 
+});
+
+export default RootReducer;
