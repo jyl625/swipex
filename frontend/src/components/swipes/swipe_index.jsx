af49a5fec99c6fc8a6477c7e6c549a10d8fe5f3b
Alan Chiu <ms0372631@gmail.com>
2021-12-07 00:15:11 -0800

    SwipeIndex and SwipeIndexItem

---
+++ frontend/src/components/swipes/swipe_index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import SwipeIndexItem from './swipe_index_item';
+
+class SwipeIndex extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.requestSwipes();
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          this.props.swipes.map((swipe, idx) => {
+            <li key={idx}>
+              <SwipeIndexItem swipe={swipe}/>
+            </li>
+          })
+        }
+      </div>
+    )
+  }
+}
+
+export default SwipeIndex;
---
+++ frontend/src/components/swipes/swipe_index_item.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SwipeIndexItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount()
+
+  render() {
+    return 
+  }
+}
