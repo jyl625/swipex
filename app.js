934a021914cf0b22fa31a9589339d98bc261c02b
Alan Chiu <ms0372631@gmail.com>
2021-12-07 09:55:47 -0800

    Create CafeteriaReducer

--- app.js
+++ app.js
@@ -27,5 +27,5 @@ app.use("/api/cafeterias", cafeterias);
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
