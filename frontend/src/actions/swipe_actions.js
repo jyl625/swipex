4d58751b8c42e47341db902d8a4956fef26a93d5
Alan Chiu <ms0372631@gmail.com>
2021-12-06 23:06:18 -0800

    Swipe actions

--- frontend/src/actions/swipe_actions.js
+++ frontend/src/actions/swipe_actions.js
@@ -1,7 +1,37 @@
-import { requestSwipe, requestSwipes, createSwipe } from "../util/swipe_api_util";
+import { fetchSwipes, fetchSwipe, createSwipe } from "../util/swipe_api_util";
 
 export const RECEIVE_SWIPES = "RECEIVE_SWIPES";
 export const RECEIVE_SWIPE = "RECEIVE_SWIPE";
-export const CREATE_
 
-export const receiveSwipes = swipes
+export const receiveSwipes = swipes => ({
+  type: RECEIVE_SWIPES,
+  swipes
+})
+
+export const receiveSwipe = swipe => ({
+  type: RECEIVE_SWIPE,
+  swipe
+}) 
+
+export const createSwipe = swipe => ({
+  type: CREATE_SWIPE,
+  swipe
+})
+
+export const requestSwipes = () => dispatch => (
+  fetchSwipes()
+    .then(swipes => dispatch(receiveSwipes(swipes)))
+    .fail(err => console.log(err))
+)
+
+export const requestSwipe = swipeId => dispatch => (
+  fetchSwipe(swipeId)
+    .then(swipe => dispatch(receiveSwipe(swipe)))
+    .fail(err => console.log(err))
+)
+
+export const createSwipe = swipeData => dispatch => (
+  createSwipe(swipeData)
+    .then(swipe => dispatch(receiveSwipe(swipe)))
+    .fail(err => console.log(err))
+)
