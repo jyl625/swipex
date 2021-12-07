b8f4bf112a1f93f43d790fd29fc97db572443a33
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-12-06 18:49:02 -0800

    Set up front end login/signup

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer';
 import errors from './errors_reducer'; 
-import swipes from './swipes_reducer'
+// import swipes from './swipes_reducer'
 
 const RootReducer = combineReducers({
   session,
   errors, 
-  swipes 
+  // swipes 
 });
 
 export default RootReducer;
