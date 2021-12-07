e6f3179170cc62f179ae18c6a7ba19d1b77e4fa1
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-12-06 21:49:06 -0800

    Import path from express and make other changes to app.js and package.json

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const cafeterias = require('./routes/api/cafeterias')
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
+const path = require('path');
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 
 mongoose
