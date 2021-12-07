cfaffe2682e10381b6f908a0967e5061de165e3b
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-12-07 10:52:55 -0800

    Add testing comments

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
+//testing
 import { Route, Switch } from 'react-router-dom';
+//testing
 import NavBarContainer from './nav/navbar_container';
 
 // import TweetsContainer from './tweets/tweets_container';
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
+//testing
 import SwipeShowContainer from './swipes/swipe_show_container';
+//testing
 import SwipeIndexContainer from './swipes/swipe_index_container';
+//testing
 // import ProfileContainer from './profile/profile_container';
 // import TweetComposeContainer from './tweets/tweet_compose_container';
 
