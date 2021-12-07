da1b699a90ef4f38e3ba86bfe19a9f0c75f2da9b
Alan Chiu <ms0372631@gmail.com>
2021-12-07 11:53:12 -0800

    Modified UserShowContainer

--- frontend/src/components/users/user_show.jsx
+++ frontend/src/components/users/user_show.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import ThreadIndexContainer from '../threads/thread_index_container';
 
 const UserShow = () => (
   <div>
-    
+    <div>
+      
+    </div>
+    <ThreadIndexContainer/>
+    {/* SwipeSaleIndexContainer */}
   </div>
 )
 
