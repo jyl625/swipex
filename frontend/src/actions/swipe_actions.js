d067fd402448cc6c041eb7ec3d62b64ec874e57c
Alan Chiu <ms0372631@gmail.com>
2021-12-06 23:30:39 -0800

    Created SwipesReducer

--- frontend/src/actions/swipe_actions.js
+++ frontend/src/actions/swipe_actions.js
@@ -1,17 +1,23 @@
 import { fetchSwipes, fetchSwipe, createSwipe } from "../util/swipe_api_util";
 
 export const RECEIVE_SWIPES = "RECEIVE_SWIPES";
-export const RECEIVE_SWIPE = "RECEIVE_SWIPE";
+export const RECEIVE_NEW_SWIPE = "RECEIVE_NEW_SWIPE";
+export const RECEIVE_USER_SWIPES = "RECEIVE_USER_SWIPES"
 
 export const receiveSwipes = swipes => ({
   type: RECEIVE_SWIPES,
   swipes
 })
 
-export const receiveSwipe = swipe => ({
-  type: RECEIVE_SWIPE,
+export const receiveNewSwipe = swipe => ({
+  type: RECEIVE_NEW_SWIPE,
   swipe
 }) 
+
+export const receiveUserSwipes = swipes => ({
+  type: RECEIVE_USER_SWIPES,
+  swipes
+})
 
 
 export const requestSwipes = () => dispatch => (
--- frontend/src/reducers/swipe_reducer.js
+++ frontend/src/reducers/swipe_reducer.js
@@ -1,15 +1,17 @@
 
-import { RECEIVE_SWIPE, RECEIVE_SWIPES } from "../actions/swipe_actions";
+import { RECEIVE_NEW_SWIPE, RECEIVE_SWIPES, RECEIVE_USER_SWIPES } from "../actions/swipe_actions";
 
-const SwipesReducer = (state = { all: {}, swpie: undefined}, action) => {
+const SwipesReducer = (state = { all: {}, user: {}, new: undefined}, action) => {
   Object.freeze(state);
   const nextState = Object.assign({}, state);
   switch(action.type) {
     case RECEIVE_SWIPES:
       nextState.all = action.swipes.data;
       return nextState;
-    case RECEIVE_SWIPE:
-      nextState[action.swipe.id] = action.swipe;
+    case RECEIVE_USER_SWIPES:
+      nextState.user = action.swipes.data;
+    case RECEIVE_NEW_SWIPE:
+      nextState.new = action.swipe.data;
       return nextState;
     default:
       return state;
