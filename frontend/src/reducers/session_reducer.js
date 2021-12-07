3a21d7337d49de0a351e9c4154fe3dd9d888447a
shuang zheng <shuangzheng16@gmail.com>
2021-12-07 15:27:08 -0800

    fix validation

---
+++ frontend/src/reducers/session_reducer.js
@@ -0,0 +1,28 @@
+import { RECEIVE_CURRENT_USER, RECEIVE_USER_LOGOUT, RECEIVE_USER_SIGN_IN } from '../actions/session_actions';
+
+const initialState = {
+  isAuthenticated: false,
+  user: {}
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case RECEIVE_USER_LOGOUT:
+      return {
+        isAuthenticated: false,
+        user: undefined
+      };
+    case RECEIVE_CURRENT_USER: //added
+      return {
+        isAuthenticated: true,
+        user: action.currentUser
+      }
+    case RECEIVE_USER_SIGN_IN: //added
+      return {
+        isAuthenticated: true,
+        user: action.currentUser
+      }
+    default:
+      return state;
+  }
+}
