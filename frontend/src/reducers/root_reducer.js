e211d171af5350f4f3294992f557f1d13dd40b57
Alan Chiu <ms0372631@gmail.com>
2021-12-06 23:32:41 -0800

    Added swipeReducer to rootReducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,12 +1,13 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer';
 import errors from './errors_reducer'; 
+import swipes from './swipe_reducer';
 // import swipes from './swipes_reducer'
 
 const RootReducer = combineReducers({
   session,
   errors, 
-  // swipes 
+  swipes 
 });
 
 export default RootReducer;
