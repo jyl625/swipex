d78c3c1329387f231bf61fc7b3b8e3f9bbe0a194
jh177 <jameshong177@gmail.com>
2021-12-07 21:51:24 -0800

    Add model and routes for exchanges

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const users = require("./routes/api/users");
 const cafeterias = require('./routes/api/cafeterias')
 const sellposts = require('./routes/api/sellposts')
 const conversations = require('./routes/api/conversations');
-const comments = require("./routes/api/comments")
+const comments = require("./routes/api/comments");
+const exchanges = require("./routes/api/exchanges")
 
 const bodyParser = require('body-parser');
 const passport = require('passport');
@@ -29,7 +30,8 @@ app.use("/api/users", users);
 app.use("/api/cafeterias", cafeterias);
 app.use("/api/sellposts", sellposts);
 app.use("/api/conversations", conversations);
-app.use("/api/comments", comments)
+app.use("/api/comments", comments);
+app.use("/api/exchanges", exchanges);
 
 
 
---
+++ routes/api/exchanges.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Exchange = require("../../models/Exchange");
+const SellPost = require("../../models/Sellpost");
+
+//testing get all exchanges
+router.get('/', (req, res) => {
+  Exchange.find()
+    .sort({ date: -1 })
+    .then(exchanges => res.json(exchanges))
+    .catch(err => res.status(404).json({ noexchangesfound: 'No exchanges found' }));
+});
+
+//get exchange by exchangeId
+router.get('/:id', (req, res) => {
+  Exchange.findById(req.params.id)
+    .then(exchange => res.json(exchange))
+    .catch(err =>
+      res.status(404).json({ noexchangefound: 'No exchange found with that ID' })
+    );
+});
+
+
+//create exchange
+router.post('/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    // validate price input?
+
+    const newExchange = new Exchange({
+      closePrice: req.body.closePrice,
+      sellPost: req.body.sellPost,
+      seller: req.body.seller,
+      buyer: req.body.buyer
+    });
+
+    newExchange.save()
+      .then(SellPost.findById(req.body.sellPost)
+        .then(sellPost => {
+          // conversation.timeUpdated = new Date();
+          sellPost.open = false;
+          sellPost.save();
+        })
+      )
+      .then(exchange => res.json(exchange));
+  }
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
