fa6d4d46852037c1717dc205fc5cfa4fd900c3cc
Jean Youn Lee <69123522+jyl625@users.noreply.github.com>
2021-12-09 11:42:32 -0800

    Organize cafeteria show page into panels

--- frontend/src/components/cafeterias/cafeteria_show.jsx
+++ frontend/src/components/cafeterias/cafeteria_show.jsx
@@ -41,14 +41,42 @@ class CafeteriaShow extends React.Component {
       }
       return cafeterias.map((cafeteria, idx) => {
         return (
-          <div className="cafeteria-container" key={idx}>
-            <div className="cafeteria-name">
-              {/* {cafeteria.location} - {cafeteria.name.toUpperCase()} */}
-              {cafeteria.name.toUpperCase()}
+          <div className="panel-container">
+            <div className="left-pannel">
+              <div className="cafeteria-container" key={idx}>
+                <div className="cafeteria-name-container">
+                  <div className="cafeteria-name-wrapper">{cafeteria.name.toUpperCase()}</div>
+                </div>
+                <SwipeIndexContainer cafeteria={cafeteria} key={idx}/>
+              </div>
             </div>
-            <SwipeIndexContainer cafeteria={cafeteria} key={idx}/>
+            <div className="right-panel">
+              <div className="map-container"></div>
+              <div className="menu-container">
+                <ul>Breakfast
+                  <li>Item 1</li>
+                  <li>Item 2</li>
+                  <li>Item 3</li>
+                  <li>Item 4</li>
+                  <li>Item 5</li>
+                </ul>
+                <ul>Lunch
+                  <li>Item 1</li>
+                  <li>Item 2</li>
+                  <li>Item 3</li>
+                  <li>Item 4</li>
+                  <li>Item 5</li>
+                </ul>
+                <ul>Dinner
+                  <li>Item 1</li>
+                  <li>Item 2</li>
+                  <li>Item 3</li>
+                  <li>Item 4</li>
+                  <li>Item 5</li>
+                </ul>
+              </div>
+            </div>
           </div>
-          
         )
       })
     } else {
@@ -61,9 +89,10 @@ class CafeteriaShow extends React.Component {
     if (this.props.cafeterias.length > 0) {
       return (
         <div className="cafeteria-show-page">
-          {
-            this.listCafeteriaSwipes()
-          }
+          <div className="cafeteria-banner">
+            <img src="https://www.food-management.com/sites/food-management.com/files/styles/article_featured_retina/public/UCLA-dining_0.jpg?itok=vkDGYyJj" alt="cafeteria banner" />
+          </div>
+          {this.listCafeteriaSwipes()}
         </div>
       )
     } else {
