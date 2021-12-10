7330545b3f5ec73334e026421d9e440545db91fe
jh177 <jameshong177@gmail.com>
2021-12-09 23:07:38 -0800

    Add footer

---
+++ frontend/src/components/team/team.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FaGithub, FaLinkedin} from "react-icons/fa";
+
+const Team = () => {
+  return(
+    <div>
+      Meet our Team!
+    </div>
+  )
+}
+
+export default Team;
