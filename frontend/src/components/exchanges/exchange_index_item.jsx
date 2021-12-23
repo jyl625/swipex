2f50cce1c6e35a33ac7e580c1ce9c8483445f98e
Jean Youn Lee <69123522+jyl625@users.noreply.github.com>
2021-12-22 20:41:14 -0800

    Add more console log for debugging

--- frontend/src/components/exchanges/exchange_index_item.jsx
+++ frontend/src/components/exchanges/exchange_index_item.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
-const ExchangeIndexItem = ({exchange}) => (
-  <div className="messages">
-    {/* <div><span className="usershow-index-item-grey">SellPost:</span> {exchange.sellPost}</div> */}
-    <div><span className="usershow-index-item-grey">Buyer:</span> {exchange.buyer}</div>
-    <div><span className="usershow-index-item-grey">Closed at:</span> ${Number(exchange.closePrice).toFixed(2)}</div>
-    <div><span className="usershow-index-item-grey">Last updated at:</span> {exchange.updatedAt}</div>
-  </div>
-)
+const ExchangeIndexItem = ({exchange}) => {
+  console.log(exchange)
+  return (
+    <div className="messages">
+      {/* <div><span className="usershow-index-item-grey">SellPost:</span> {exchange.sellPost}</div> */}
+      <div><span className="usershow-index-item-grey">Buyer:</span> {exchange.buyer}</div>
+      <div><span className="usershow-index-item-grey">Closed at:</span> ${Number(exchange.closePrice).toFixed(2)}</div>
+      <div><span className="usershow-index-item-grey">Last updated at:</span> {exchange.updatedAt}</div>
+    </div>
+  )
+}
 
 export default ExchangeIndexItem;
