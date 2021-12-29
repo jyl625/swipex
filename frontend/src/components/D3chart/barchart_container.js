9486cb137b890cdfcf81906c3e210da1ffb84deb
jh177 <jameshong177@gmail.com>
2021-12-28 18:57:59 -0800

    Edit charts in cafe show to rerender upon changing cafes

--- frontend/src/components/D3chart/barchart_container.js
+++ frontend/src/components/D3chart/barchart_container.js
@@ -1,10 +1,13 @@
 import { connect } from 'react-redux';
 import { requestExchanges} from '../../actions/exchange_actions';
 import BarchartData from './barchart_data'
+import { withRouter } from 'react-router-dom';
+
 
 const mapStateToProps = (state, ownProps) => ({
   exchanges: Object.values(state.exchanges.all),
-  cafeSwipes: ownProps.cafeSwipes
+  cafeSwipes: ownProps.cafeSwipes,
+  cafeName: ownProps.match.params.cafeteriaName
   // cafeExchanges: (() => {
   //   const cafeSwipes = Object.values(ownProps.cafeSwipes);
   //   const cafeSwipesPostId = [];
@@ -19,7 +22,7 @@ const mapDispatchToProps = dispatch => ({
   requestExchanges: () => dispatch(requestExchanges())
 })
 
-export default connect(
+export default withRouter(connect(
   mapStateToProps,
   mapDispatchToProps
-)(BarchartData);
+)(BarchartData));
--- frontend/src/components/D3chart/linechart_container.js
+++ frontend/src/components/D3chart/linechart_container.js
@@ -1,17 +1,19 @@
 import { connect } from 'react-redux';
 import { requestExchanges } from '../../actions/exchange_actions';
 import LinechartData from './linechart_data';
+import { withRouter } from 'react-router-dom';
 
 const mapStateToProps = (state, ownProps) => ({
   exchanges: Object.values(state.exchanges.all),
-  cafeSwipes: ownProps.cafeSwipes
+  cafeSwipes: ownProps.cafeSwipes,
+  cafeName: ownProps.match.params.cafeteriaName
 });
 
 const mapDispatchToProps = dispatch => ({
   requestExchanges: () => dispatch(requestExchanges())
 })
 
-export default connect(
+export default withRouter(connect(
   mapStateToProps,
   mapDispatchToProps
-)(LinechartData);
+)(LinechartData));
